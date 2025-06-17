1dfacf36e1bea217cb3c179b106ef2d2eedbca95
Igor Zlobinsky <igorz@comda.co.il>
2025-06-17 08:03:48 +0300

    Unnecessary middleware code was neutralized

--- EsignBackend/Middlewares/AddTokenFromCookieMiddleware.cs
+++ EsignBackend/Middlewares/AddTokenFromCookieMiddleware.cs
@@ -21,11 +21,35 @@ namespace EsignBackend.Middlewares
         public async Task InvokeAsync(HttpContext context)
         {
             var tokenCookie = context.Request.Cookies["accessToken"];
+
+    //    Header:
+    //        {
+    //            "alg": "HS256",
+		  //"typ": "JWT"
+
+    //    }
+
+    //    Payload:
+    //        {
+    //            "nameid": "340",
+		  //"unique_name": "igorz",
+		  //"role": "מנהל",
+		  //"nbf": 1749986224,
+		  //"exp": 1749993424,
+		  //"iat": 1749986224
+
+    //    }
+
+    //    Signature:
+    //        9bQSushwr_FGbcNs1S6Muhk0f9BnRV4OrcSH5vO34Bs
+
             var authHeaderExist = !string.IsNullOrEmpty(context.Request.Headers["Authorization"]);
+
+            // has no effect on the given call (the only useful outcome is investigating token structure)
 
             if (!authHeaderExist && tokenCookie != null)
             {
-                context.Request.Headers.Add("Authorization", $"Bearer: {tokenCookie}");
+                //context.Request.Headers.Add("Authorization", $"Bearer: {tokenCookie}");
             }
 
             await next(context);
